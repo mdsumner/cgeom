e347e7e78f6b7311298069b1c26355a43307b533
Michael Sumner <mdsumner@gmail.com>
2016-02-21 15:45:24 +1100

    first pass chull

--- R/cgeom.r
+++ R/cgeom.r
@@ -1,13 +1,9 @@
 
-vcrossp <- function( a, b ) {
+vcrossp <- function(a, b) {
   cbind(a[,2] * b[,3] - a[,3] * b[,2],
         a[,3] * b[,1] - a[,1] * b[,3],
         a[,1] * b[,2] - a[,2] * b[,1])
 }
-
-library(rbenchmark)
-
-benchmark(vcrossp(pts[-1, ], pts[-nrow(pts), ]), vcrossp1(pts[-1, ], pts[-nrow(pts), ]),  replications = 10000)
 
 
 mkm <- function(x, n) {
@@ -24,39 +20,17 @@ SameSide <- function(p1,p2, a,b) {
   c(cp1 %*% t(cp2)) >= 0
 }
 
-vecrize <- TRUE
-set.seed(1)
-pts <- cbind(matrix(rnorm(150) , ncol = 2), 0)
-
-
-plot(pts)
-system.time({
-  edges <- t(combn(seq(nrow(pts)), 2))
-  edgetest <- logical(nrow(edges))
-
-  for (ie in seq(nrow(edges))) {
-    edge <- pts[edges[ie, ], ]
-    #lines(edge, col = rgb(1, 0, 0, 0.3))
-    kpts <- setdiff(seq(nrow(pts)), edges[ie, ])
-    if (vecrize) {
-
-      test <- SameSide(pts[kpts[-1], , drop = FALSE], pts[kpts[-length(kpts)], , drop = FALSE],
-                       edge[1, , drop = FALSE], edge[2, , drop = FALSE])
-    } else {
-      test <- TRUE
-      for (k in seq(length(kpts) -1)) {
-        test <- test && SameSide(pts[kpts[k], , drop = FALSE], pts[kpts[k + 1], , drop = FALSE],
-                                 edge[1, , drop = FALSE], edge[2, , drop = FALSE])
-      }
-
-    }
-    if (all(test)) {
-      edgetest[ie] <- TRUE
-      #lines(edge, lty =2, lwd = 3)
-    }
-
-  }
-
-})
-
-apply(edges[edgetest, ], 1, function(x) lines(pts[x, ], lwd = 4))
+vlen <- function(x) {
+  sqrt(diff(x[, 1])^2 + diff(x[, 2])^2 + diff(x[, 3])^2)
+}
+angle <- function(x) {
+  x1 <- x[1,] - x[2,]
+  x2 <- x[1,] - x[3,]
+  a <- (atan2(x1[2], x1[1]) - atan2(x2[2], x2[1]))
+  if (a < -pi) a<- a + 2*pi
+  if (a > pi) a <- a- 2*pi
+  a
+}
+turnright <- function(x) {
+  !angle(x) < 0
+}
