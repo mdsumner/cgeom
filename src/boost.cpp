3e8171f2eb535ac29b312a9dfb55eac4ca1a1263
Michael Sumner <mdsumner@gmail.com>
2017-02-24 23:14:25 +1100

    minimal working boost

--- src/boost.cpp
+++ src/boost.cpp
@@ -6,17 +6,34 @@
 
 using namespace Rcpp;
 
+//' Boilerplate Rcpp.  
+//' 
+//' Plate boiler. 
+//' @param a number
+//' @return b nice
+//' @export
+//' @examples
+//' \dontrun{
+//' a <- 6
+//' b <- 15
+//' cat( c(computeGCD(a,b), computeLCM(a,b)), "\n")
 // [[Rcpp::export]]
 int computeGCD(int a, int b) {
   return boost::math::gcd(a, b);
 }
 
+//' Boilerplate Rcpp.  
+//' 
+//' Plate boiler. 
+//' @param a number
+//' @return b nice
+//' @export
+//' @examples
+//' \dontrun{
+//' a <- 6
+//' b <- 15
+//' cat( c(computeGCD(a,b), computeLCM(a,b)), "\n")
 // [[Rcpp::export]]
 int computeLCM(int a, int b) {
   return boost::math::lcm(a, b);
 }
-We can test these:
-  
-  a <- 6
-b <- 15
-cat( c(computeGCD(a,b), computeLCM(a,b)), "\n")
